14a67c6ee788a3e37672e8725401b14f17970287
Erick Goldberg <erick_goldberg@hotmail.com>
2023-06-06 07:20:13 -0300

    creating routes and form components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import User from './pages/User';
 import NotFound from './pages/notFound';
+import Home from './pages/Home';
+import Events from './pages/Events'
+import Cashiers from './pages/Cashiers'
+import Chamados from './pages/Chamados'
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<User />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/cashiers" element={<Cashiers />} />
+        <Route path="/chamados" element={<Chamados />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
